291926637a8c2ce15140c6c0acff1e12d24eedd0
reddexx <killmaster-loc@hotmail.de>
2024-11-29 14:26:24 +0100

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,12 +2,6 @@
 
 [ -z "${DEBUG}" ] || set -x
 
-# Download the installer
-curl -sL ${BASE_URL:-https://jmlemetayer.github.io/abba}/install -o /tmp/install
-
-# And run it in the workdir
-/bin/sh -e ${DEBUG:+-x} /tmp/install ${THEME}
-
 # Uninstall on exit
 trap "" EXIT
 
