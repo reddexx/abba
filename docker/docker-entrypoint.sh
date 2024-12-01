a368f3b4ef1c73cfc3da674d43155ff7bce36500
reddexx <killmaster-loc@hotmail.de>
2024-12-01 23:15:19 +0100

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,7 +8,7 @@ CHECK_FILE="/var/www/.abba/uninstall" # Ersetze dies mit dem tatsächlichen Pfad
 # Überprüfe, ob die Datei vorhanden ist
 if [ ! -f "$CHECK_FILE" ]; then
     # Download the installer
-    curl -sL ${BASE_URL:-https://jmlemetayer.github.io/abba}/install -o /tmp/install
+    curl -sL ${BASE_URL:-https://github.com/reddexx/abba/blob/main/}/install -o /tmp/install
 
     # And run it in the workdir
     /bin/sh -e ${DEBUG:+-x} /tmp/install ${THEME}
